90c61c324752fcdf940f9b00405b27b815f03478
GitAndyUser <github@genge.co.uk>
2026-06-14 15:53:25 +0100

    Changes to state to enable KMS enrypt

--- terraform_test/backend.tf
+++ terraform_test/backend.tf
@@ -1,8 +1,12 @@
+#set up a remote state file so code can be executed by any admins. 
+#added KMS key so it can be run by dev teams. 
+
 terraform {
   backend "s3" {
     bucket = "terraform-state-bucket"
     key    = "/dev/path/state/terraform.tfstate"
     region = "eu-west-2"
     encrypt = true
+    kms_key_id = "arn:aws:kms:eu-west-2:123456789012:key/blah-1234..."
   }
 }
